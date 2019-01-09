62d5414201f588385aa81fa03c78aadb9b05a5fb
codekongs <szh@codekong.cn>
2019-01-09 23:36:40 +0800

    完成自定义View练习大部分题目

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,22 +2,35 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
-
+    private Paint mPaint;
+    private Path mHeartPath;
     public Practice9DrawPathView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+
+        mHeartPath = new Path();
+        mHeartPath.addArc(300, 200, 450, 350, -225, 225);
+        mHeartPath.arcTo(450, 200, 600, 350, -180, 225, false);
+        mHeartPath.lineTo(450, 470);
     }
 
     @Override
@@ -25,5 +38,6 @@ public class Practice9DrawPathView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+        canvas.drawPath(mHeartPath, mPaint);
     }
 }
